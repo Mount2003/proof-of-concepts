bfabcd3c3b629b0a618533f67f04c698765e4859
Maegan Maulder <maeganmaulder420@gmail.com>
2025-08-17 21:21:25 +0800

    update main.py code; rename sample-data file name

--- automatic-form-filler/main.py
+++ automatic-form-filler/main.py
@@ -3,6 +3,15 @@ import re
 import requests
 import dotenv
 import os
+import json
+import pprint
+
+def replace_PlaceHolder(doc):
+    test = re.compile(r'\{\{AnalystName\}\}')
+    for para in doc.paragraphs:
+        if test.search(para.text):
+            para.text = test.sub('Maegan', para.text)
+    for para in doc.paragraphs: print(para.text)
 
 def get_fileInfoByHash(fileSha1Hash, VT_API_KEY):
     url = f"https://www.virustotal.com/api/v3/files/{fileSha1Hash}"
@@ -12,6 +21,10 @@ def get_fileInfoByHash(fileSha1Hash, VT_API_KEY):
     }
     response = requests.get(url, headers=headers)
     return response.json()
+
+def get_sampleData():
+    with open('sample-data.json','r') as f:
+        return json.load(f)
 
 def get_placeHolders(doc):
     holders = []
@@ -30,7 +43,9 @@ def get_env(secret):
 
 def main():
     doc = docx.Document("malware_report_template.docx")
-    # apiResult = get_fileInfoByHash(get_env("FILE_SHA1SUM"), get_env("VIRUS_TOTAL_API_KEY"))
+    # apiResult = get_fileInfoByHash(get_env("FILE_SHA1SUM"), get_env("VIRUS_TOTAL_API_KEY")) # Fetch real data
+    # pprint.pprint(get_sampleData()) # Fetch sample data
+    replace_PlaceHolder(doc)
 
 if __name__ == "__main__":
     main()
